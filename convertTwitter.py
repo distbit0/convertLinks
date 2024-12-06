f40e71a478140b45721ce937730fb274039be05b
Dist Bit <adp01@pm.me>
2024-12-06 13:58:53 +0700

    convertTwitter.py

--- convertTwitter.py
+++ convertTwitter.py
@@ -408,7 +408,7 @@ def json_to_html(json_data, topTweet, op_username):
         #     tweet["children"],
         #     key=lambda x: get_longest_chain_length(x, json_data),
         #     reverse=True
-        # ) ###this is 100% depth first. a bit to extreme imo, because it makes other replies less contexualised
+        # ) ###this is 100% depth first. a bit to extreme imo, because it makes other replies less contexualised. maybe some middle ground is optimal.
         sorted_children = tweet["children"]
         for childId in sorted_children:
             outStr += convert_to_html(childId, level + 1)
